276f77ac6cfbcd9247cf92988da29741d7a83403
Vitor Santos <vitor.sannctorum@hotmail.com>
2024-03-18 12:47:51 -0300

    Update: User update configured & Code formated

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
-import { knex } from "../database/knex";
-import { AppError } from "../utils/AppError";
-import { hash, compare } from "bcryptjs";
+import { Request, Response } from 'express';
+import { knex } from '../database/knex';
+import { AppError } from '../utils/AppError';
+import { hash, compare } from 'bcryptjs';
 
 interface UserData {
   name: string;
@@ -32,29 +32,26 @@ export class UserController {
     }
 
     if (!name || !email || !password) {
-      throw new AppError("You must enter all the data to create an account.");
+      throw new AppError('You must enter all the data to create an account.');
     }
 
     if (!isEmailValid(email)) {
-      throw new AppError("The email is not valid.");
+      throw new AppError('The email is not valid.');
     }
 
     if (!isPasswordValid(password)) {
-      throw new AppError("The password is not valid.");
+      throw new AppError('The password is not valid.');
     }
 
-    const emailAlreadyExists = await knex("users")
-      .select("*")
-      .where({ email })
-      .first();
+    const emailAlreadyExists = await knex('users').select('*').where({ email }).first();
 
     if (emailAlreadyExists) {
-      throw new AppError("Email already exists.");
+      throw new AppError('Email already exists.');
     }
 
     const hashedPassword = await hash(password, 8);
 
-    await knex("users").insert({
+    await knex('users').insert({
       name,
       email,
       password: hashedPassword,
@@ -62,8 +59,8 @@ export class UserController {
     });
 
     return response.status(201).json({
-      status: "success",
-      message: "User created!",
+      status: 'success',
+      message: 'User created!',
     });
   }
 
@@ -71,23 +68,17 @@ export class UserController {
     const { name, email, password, old_password }: UserData = request.body;
     const { id } = request.params;
 
-    const user: UserDatabaseType = await knex("users")
-      .select("*")
-      .where({ id })
-      .first();
+    const user: UserDatabaseType = await knex('users').select('*').where({ id }).first();
 
     if (!user) {
-      throw new AppError("User does not exist.");
+      throw new AppError('User does not exist.');
     }
 
     if (email) {
-      const userWithUpdatedEmail = await knex("users")
-        .select("*")
-        .where({ email })
-        .first();
+      const userWithUpdatedEmail = await knex('users').select('*').where({ email }).first();
 
       if (userWithUpdatedEmail && userWithUpdatedEmail.id != id) {
-        throw new AppError("E-mail already in use.");
+        throw new AppError('E-mail already in use.');
       }
     }
 
@@ -95,27 +86,30 @@ export class UserController {
     user.email = email ?? user.email;
 
     if (password && !old_password) {
-      throw new AppError("Old password is mandatory.");
+      throw new AppError('Old password is mandatory.');
     }
 
     if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
       if (!checkOldPassword) {
-        throw new AppError("Old Password is invalid.");
+        throw new AppError('Old Password is invalid.');
       }
-      /*if(checkOldPassword) {
-        throw new AppError("Old password is equal new password.")
-      }*/
+
+      const checkNewPassword = await compare(password, user.password);
+      if (checkNewPassword) {
+        throw new AppError('Old password is equal new password.');
+      }
+
       user.password = await hash(password, 8);
     }
 
-    await knex("users")
-      .update({ name, email, password, updated_at: knex.fn.now() })
+    await knex('users')
+      .update({ name, email, password: user.password, updated_at: knex.fn.now() })
       .where({ id });
 
     response.status(202).json({
-      status: "success",
-      message: "User are updated",
+      status: 'success',
+      message: 'User are updated',
     });
   }
 }
